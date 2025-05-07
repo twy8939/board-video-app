58731ce8819363eae3585357cbf5ffa6cca8a3e7
twy8939 <twy8939@naver.com>
2025-05-07 19:54:01 +0900

    init : Add Convex client provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClientProvider from "@/providers/convex-client-provider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+        <ClerkProvider>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
